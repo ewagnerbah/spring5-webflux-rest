5f91809e8741ff61129e85a03db86384485f7f46
Edward Wagner <wagner_edward@bah.com>
2022-04-07 12:08:45 -0400

    Video 411

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -13,9 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Category {
+public class Vendor {
     @Id
     private String id;
 
-    private String description;
+    private String firstName;
+    private String lastName;
 }
